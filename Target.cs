07d43d506f4db879f43f308d3961a002df7dfc48
thiliporace <85422792+thiliporace@users.noreply.github.com>
2023-05-09 20:52:15 +0000

    Initial commit

---
+++ Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    public GameObject Enemy;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(Enemy);
+    }
+}
